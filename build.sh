3d05274c49001c959d547f03c81ef4c56f024627
Mohammad Danish Sheikh <sheikhd811@gmail.com>
2024-10-03 00:36:12 +0530

    first commit

--- build.sh
+++ build.sh
@@ -1,9 +1,4 @@
-
-sudo apt-get update
 sudo apt-get install -y wget gnupg
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-sudo apt-get update
 sudo apt-get install -y google-chrome-stable
 
 #!/bin/bash
@@ -22,9 +17,7 @@ sudo apt-get update && sudo apt-get install -y \
 sudo rm -rf /var/lib/apt/lists/*
 
 # Install Google Chrome
-wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
-sudo apt-get update && sudo apt-get install -y google-chrome-stable
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub
 
 # Install ChromeDriver
 wget -O /tmp/chromedriver.zip https://storage.googleapis.com/chrome-for-testing-public/129.0.6668.89/linux64/chromedriver-linux64.zip
@@ -33,7 +26,6 @@ rm /tmp/chromedriver.zip
 
 # Ensure pip is installed and upgrade to the latest version
 python3 -m ensurepip --upgrade
-pip install --upgrade pip
 
 # Install Python dependencies from requirements.txt
 pip install -r requirements.txt
